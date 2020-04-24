3bd85a8937a3d4ce7f0c47f4799641f9c3d4ac12
Vipul Barodiya <sonumeewa@gmail.com>
2020-04-25 04:55:52 +0530

    site title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Vipul Barodiya';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Vipul Barodiya | Blog';
 
 export default function Layout({ children, home }) {
   return (
